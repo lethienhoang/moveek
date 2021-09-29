2522863955646a72f39e513869d6e98f092201e2
lethienhoang <45428011+lethienhoang@users.noreply.github.com>
2021-09-29 14:20:46 +0700

    add css, themes,..
    implement layout

---
+++ Moveek.Admin/Controllers/FilmsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Moveek.Admin.Controllers
+{
+    public class FilmsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Category()
+        {
+            return View();
+        }
+
+        public IActionResult Rating()
+        {
+            return View();
+        }
+    }
+}
